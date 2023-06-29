66c18ad9f3365b8210c15c1392fe579bb978741e
John Rosak <42981756+RosakJ@users.noreply.github.com>
2023-06-29 02:04:45 -0400

    Upload of the first section of the website

---
+++ src/js/script.js
@@ -0,0 +1,11 @@
+disableScroll()
+
+// Disable scroll
+function disableScroll() {
+  document.body.style.overflow = "hidden";
+}
+
+// Enable scroll
+function enableScroll() {
+  document.body.style.overflow = "auto";
+}
